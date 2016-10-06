af06cfc1127c78c8ebf62aadf74f9a8afe4962df
Stanislav Kondrashkin <stanislav.kondrashkin@isbank.com.ru>
2016-10-06 17:56:35 +0300

    ex 2 page 96

--- src/Ex1P96.java
+++ src/Ex1P96.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 /**
  * Created by sk10271 on 06.10.2016.
  */
 public class Ex1P96 {
     public static void main(String[] args){
-        ex1();
+        //ex1();
+        ex2();
     }
 
     public static void ex1(){
@@ -11,4 +14,21 @@ public class Ex1P96 {
             System.out.println(i);
         }
     }
+
+    public static void ex2(){
+        Random rand = new Random();
+        int i1, i2, i3, i4, i5;
+        i1 = rand.nextInt();
+        i2 = rand.nextInt();
+        i3 = rand.nextInt();
+        i4 = rand.nextInt();
+        i5 = rand.nextInt();
+        if (i1>i2) System.out.println("i1 is greater then i2!");
+        else if (i1<i2) System.out.println("i1 is less then i2!");
+        else if (i1==i2) System.out.println("i1 is equals i2!");
+
+
+    }
+
+
 }
